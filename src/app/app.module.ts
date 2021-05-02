c6d7df8eb09b106510ccd8f37643e44c8042f94b
ArnePunk <arnim.heimadinger@web.de>
2021-05-02 19:52:03 +0200

    Move add-user-button folder to table folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,8 @@ import { TableRowComponent } from 'src/table/table-row/table-row.component'
 import { UserEditComponent } from 'src/table/table-row/user-edit/user-edit.component'
 import { UserEntryComponent } from 'src/table/table-row/user-entry/user-entry.component'
 import { TableHeadComponent } from 'src/table/table-head/table-head.component'
-import { AddUserButtonComponent } from './add-user-button/add-user-button.component'
-import { AddUserModalComponent } from './add-user-button/add-user-modal/add-user-modal.component'
+import { AddUserButtonComponent } from 'src/table/add-user-button/add-user-button.component'
+import { AddUserModalComponent } from 'src/table/add-user-button/add-user-modal/add-user-modal.component'
 
 @NgModule({
     declarations: [
